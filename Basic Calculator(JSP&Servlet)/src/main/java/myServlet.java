0cf08cea5082cbcbac9eac1e8e0169dda862f3ba
Radheya-2811 <148652306+Radheya-2811@users.noreply.github.com>
2024-03-28 20:27:23 +0530

    Add files via upload

---
+++ Basic Calculator(JSP&Servlet)/src/main/java/myServlet.java
@@ -0,0 +1,61 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class myServlet
+ */
+@WebServlet("/myServlet")
+public class myServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public myServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int num1=Integer.parseInt(request.getParameter("num1"));
+		int num2=Integer.parseInt(request.getParameter("num2"));
+		String operator=request.getParameter("bt1");
+		int answer = 0;
+		switch(operator) {
+		    case "1":
+		    	answer=num1+num2;
+		    	break;
+		    case "2":
+		    	answer=num1-num2;
+		    	break;
+		    case "3":
+		    	answer=num1*num2;
+		    	break;
+		    case "4":
+		    	answer=num1/num2;
+		    	break;
+		}
+		
+		
+		response.sendRedirect("index.jsp?answer="+answer);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
